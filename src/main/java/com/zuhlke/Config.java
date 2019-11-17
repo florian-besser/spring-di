58dcccf0f2513708b86ec977d3eef9d8f9545923
Florian Besser <f.besser@hotmail.com>
2019-11-17 23:02:41 +0700

    Add spring DI example code

---
+++ src/main/java/com/zuhlke/Config.java
@@ -0,0 +1,22 @@
+package com.zuhlke;
+
+import com.zuhlke.domain.Engine;
+import com.zuhlke.domain.Transmission;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.zuhlke.domain")
+public class Config {
+
+    @Bean
+    public Engine engine() {
+        return new Engine("v8", 5);
+    }
+
+    @Bean
+    public Transmission transmission() {
+        return new Transmission("sliding");
+    }
+}
